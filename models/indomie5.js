483f373f2c5d0ad3767f86cb00285e2c4342df76
Kristiani Nainggolan <90815204+2172044@users.noreply.github.com>
2024-01-24 12:05:34 +0700

    Update indomie5.js

--- models/indomie5.js
+++ models/indomie5.js
@@ -1,6 +1,6 @@
 import { loader, scene, cam, renderer } from '../index.js';
 
-const loadIndomie5 = () => {
+const loadIndomie5 = (x_coordinate,y_coordinate, z_coordinate) => {
     const sceneIndomie5 = './assets/indomie_goreng-hypeAbis.glb';
 
     return new Promise((resolve) => {
@@ -9,8 +9,8 @@ const loadIndomie5 = () => {
             scene.add(indomieModel);
 
             // Set initial properties
-            indomieModel.position.set(-2, -0.43, -0.8);
-            indomieModel.scale.set(0.2, 0.2, 0.2);
+            indomieModel.position.set(x_coordinate,y_coordinate, z_coordinate);
+            indomieModel.scale.set(0.15, 0.15, 0.15);
             indomieModel.rotation.set(4.2, 0, 9.5);
 
             const animate = () => {
